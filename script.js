a212facdcd3e294ab52e64e70aa234ff466ab08d
Mohammad Haris <112751385+mdharis98@users.noreply.github.com>
2024-09-20 21:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+cy.get('#prev').should('not.be.disabled').click();
+cy.get('#next').click(); // Move forward, enabling the 'Prev' button.
+cy.get('#prev').should('not.be.disabled').click(); // Click the 'Prev' button once it's enabled.
+cy.get('#prev', { timeout: 6000 }).should('not.be.disabled').click();
